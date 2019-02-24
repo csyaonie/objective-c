1e25a5bdab22945966d8ee09c92b637f9bbd9ce3
csyaonie <csyaonie@qq.com>
2019-02-24 19:04:58 +0800

    Initial Commit

---
+++ ObjectcDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjectcDemo
+//
+//  Created by csyaonie on 2019/2/24.
+//  Copyright © 2019 csyaonie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
